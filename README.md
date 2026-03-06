df94d42d7ee37dbb69ba8a47d84c549823451483
KJ Roelke <kroelke@choctawnation.com>
2026-03-06 10:59:27 -0600

    init v1

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # CNHSA Content Federation
 
-This plugin is exclusively for use on the Choctaw Nation site.
+This plugin creates a two-way street, in conjunction with its sibling plugin, for federating content between the CNO and CNHSA sites. It is exclusively for use on the Choctaw Nation site.
 
-## File Overview
+## Quick Dev Notes
 
-All files live in `/inc`
+-   This plugin uses classmap autoloading, meaning **each time you add a new class, you must run `composer dump-autoload` to load it.**
+-   Classes are namespaced in PSR-4-like style. It's not perfect, but it basically conforms to the following structure:
+    -   `namespace ChoctawNation\CNHSA_Federation` points to `inc/`
+    -   Every folder after that is included in the namespace (i.e. `namespace ChoctawNation\CNHSA_Federation\Transport`)
+
+## Architecture Overview
+
+### General Overview:
+
+-   `inc/` is where PHP lives
+-   `tests/` is where the PHPUnit tests live
+-   `src/` is where the React code (for Admin Screen) lives
+
+### Terms
+
+-   `inc/WP` has files (and subdirectories) related to WordPress actions (hooks, cron events, post/payload creation, etc)
+-   `inc/Transport` is for files related to sending/receiving data over HTTP
+    -   `HTTP_Gateway` class is for sending (outbound) data
+    -   `Rest_Router` class is for receiving (inbound) data
--- cno-plugin-cnhsa-content-federation.php
+++ cno-plugin-cnhsa-content-federation.php
@@ -1,13 +1,17 @@
 <?php
 /**
- * Plugin Name:     CNHSA Content Federation
- * Plugin URI:      https://github.com/choctawnation/cno-plugin-cnhsa-content-federation
- * Description:     Allows the CNO site to federate content to and from the CNHSA site.
- * Author:          Choctaw Nation of Oklahoma
- * Author URI:      https://www.choctawnation.com
- * Version:         0.1.0
+ * Plugin Name:       CNHSA Content Federation
+ * Plugin URI:        https://github.com/choctawnation/cno-plugin-cnhsa-content-federation
+ * Description:       Allows the CNO site to federate content to and from the CNHSA site.
+ * Author:            Choctaw Nation of Oklahoma
+ * Author URI:        https://www.choctawnation.com
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Tested up to:      6.9.1
+ * Requires Plugins:  advanced-custom-fields-pro
  *
- * @package         CNHSA_Federation
+ * @package           CNHSA_Federation
  */
 
 use ChoctawNation\CNHSA_Federation\Plugin_Loader;
@@ -21,7 +25,7 @@ $cnhsa_federation_plugin = new Plugin_Loader();
 
 // Plugin Lifecycle Hooks
 register_activation_hook( __FILE__, array( $cnhsa_federation_plugin, 'activate' ) );
-register_deactivation_hook( __FILE__, array( $cnhsa_federation_plugin, 'deactivate' ) );
+
 // Static method for uninstall since the plugin can't rely on instance methods.
 register_uninstall_hook( __FILE__, array( 'ChoctawNation\CNHSA_Federation\Plugin_Loader', 'uninstall' ) );
 
